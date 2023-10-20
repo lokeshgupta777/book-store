2a0651c26c76abadb222977381aa056450fab25e
lokeshgupta777 <lokesh77793@gmail.com>
2023-10-21 03:18:25 +0530

    feat: Added books in cart count near checkout.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom"
 import styles from "./NavBar.module.css"
+import { useCart } from "../../providers/cart"
 
 const NavBar = () => {
+
+  const { booksInCart } = useCart()
+
   return (
     <nav className={styles.cont}>
       <Link to={"/"}>Home</Link>
       <Link to={"/books"}>Books</Link>
       <Link to={"/authors"}>Authors</Link>
-      <Link to={"/checkout"}>Checkout</Link>
+      <Link to={"/checkout"}>
+        <div>
+          <span>Checkout</span>
+          <span style={{ marginLeft: '5px' }}>{booksInCart?.length}</span>
+        </div>
+      </Link>
     </nav>
   )
 }
