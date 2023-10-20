028e737736bf231b78b467d91e6c3f97d8d5b4e4
lokeshgupta777 <lokesh77793@gmail.com>
2023-10-20 15:47:59 +0530

    removed old router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, RouterProvider } from "react-router-dom"
-import router from './routes/router.js';
 import AppRoutes from './routes/AppRoutes.js';
 import './index.css';
 
