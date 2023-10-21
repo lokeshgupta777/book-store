3caffe0d413f2ace60fe908e8ac3e6a246e6518b
lokeshgupta777 <lokesh77793@gmail.com>
2023-10-21 14:31:16 +0530

    feat: Sign Up added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,6 +7,7 @@ const Login = () => {
 
   const { setUserName, setUserPass, showLogin, setShowLogin } = useCart();
 
+  const [mode, setMode] = useState("login");
   const [typedUserName, setTypedUserName] = useState("");
   const [typedUserPass, setTypedUserPass] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
@@ -22,15 +23,40 @@ const Login = () => {
     }
   }, [showLogin])
 
+  const getRandomId = () => {
+    let random = 0;
+    while (!((random >= 1) && (random <= 100000))) {
+      random = Math.random() * 100000;
+    }
+    return Math.floor(random)
+  }
+
   const loginHandler = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axiosIns.get(`/users?user_name=${typedUserName}&user_pass=${typedUserPass}`);
-      if (data?.length && data[0]?.id) {
-        setUserName(data[0]?.user_name)
-        setUserPass(data[0]?.user_pass)
-        document.cookie = `user_name=${data[0]?.user_name}; path=/; max-age=86400`
-        document.cookie = `user_pass=${data[0]?.user_pass}; path=/; max-age=86400`
+      let userData = null;
+      if (mode === 'login') {
+        const { data } = await axiosIns.get(`/users?user_name=${typedUserName}&user_pass=${typedUserPass}`);
+        userData = data
+      } else {
+        let newId = getRandomId();
+        const { data } = await axiosIns({
+          method: "post",
+          url: `/users`,
+          data: {
+            id: newId,
+            user_name: typedUserName,
+            user_pass: typedUserPass,
+          },
+        })
+        userData = data
+      }
+
+      if (userData?.length && userData[0]?.id) {
+        setUserName(userData[0]?.user_name)
+        setUserPass(userData[0]?.user_pass)
+        document.cookie = `user_name=${userData[0]?.user_name}; path=/; max-age=86400`
+        document.cookie = `user_pass=${userData[0]?.user_pass}; path=/; max-age=86400`
         setShowLogin(false);
       } else {
         setErrorMsg("UserName or password incorrect!");
@@ -50,9 +76,20 @@ const Login = () => {
     setErrorMsg("")
   }
 
+  const modeToggleHandler = () => {
+    if (mode === 'login') {
+      setMode('sign up');
+      setTypedUserName("");
+      setTypedUserPass("");
+    }
+    else
+      setMode('login')
+    setErrorMsg("");
+  }
+
   return (
     <dialog id="login-modal">
-      <form onSubmit={loginHandler} className={styles.form}>
+      <form onSubmit={loginHandler} className={styles.form} autoComplete="off">
         <div className={styles.inputCont}>
           <span>Username:</span>
           <input
@@ -72,8 +109,14 @@ const Login = () => {
           />
         </div>
         {errorMsg && <p className={styles.errMsg}>{errorMsg}</p>}
-        <button type="submit" className={styles.button}>Login</button>
+        <button type="submit" className={styles.button}>{mode}</button>
       </form>
+      <p
+        onClick={modeToggleHandler}
+        className={styles.mode}
+      >
+        {mode === 'login' ? "Sign Up" : "Login"}
+      </p>
     </dialog>
   )
 }
