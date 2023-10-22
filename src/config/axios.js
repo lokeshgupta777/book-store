587dcacff326ec7473b5ef648fb7208fad02b927
lokeshgupta777 <lokesh77793@gmail.com>
2023-10-22 15:22:58 +0530

    fix: env variables, axios config

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 let baseURL = "http://localhost:3010"
-if (process.env.API_BASE_URL) {
-    baseURL = `${process.env.API_BASE_URL}`;
+if (process.env.REACT_APP_API_BASE_URL) {
+    baseURL = `${process.env.REACT_APP_API_BASE_URL}`;
 }
 const axiosIns = axios.create({
     baseURL: baseURL,
